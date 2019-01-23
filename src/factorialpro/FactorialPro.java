79b3354baf8bde03bb1c26f8bf5bc354f8703589
melakugl <melakugrm@gmail.com>
2019-01-23 17:30:16 +0000

    Initial commit

---
+++ src/factorialpro/FactorialPro.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorialpro;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melaku
+ */
+public class FactorialPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int factorial(int num)
+    {
+        if (num==0)
+            return 1;
+        else if (num==1)
+            return 1;
+        else 
+            return num*factorial(--num);
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner input=new Scanner(System.in);
+        System.out.println("please enter an integer");
+        int num=input.nextInt();
+        System.out.println(num +"!="+ factorial(num));
+    }
+    
+}
